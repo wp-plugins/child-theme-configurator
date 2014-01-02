0724d40207a5371aeb9882cdd6a2bdbbd54a140e
lilaeamedia <lilaeamedia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-02 04:42:32 +0000

    release 1.1.7 adds tutorial video to help tabs.
    git-svn-id: https://plugins.svn.wordpress.org/child-theme-configurator/tags/1.1.7@831376 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- child-theme-configurator.php
+++ child-theme-configurator.php
@@ -6,13 +6,13 @@ if ( !defined('ABSPATH')) exit;
     Plugin Name: Child Theme Configurator
     Plugin URI: http://www.lilaeamedia.com/plugins/child-theme-configurator/
     Description: Create a Child Theme from any installed Theme. Each CSS selector, rule and value can then be searched, previewed and modified.
-    Version: 1.1.6
+    Version: 1.1.7
     Author: Lilaea Media
     Author URI: http://www.lilaeamedia.com/
     Text Domain: chld_thm_cfg
     Domain Path: /lang
     License: GPLv2
-    Copyright (C) 2013 Lilaea Media
+    Copyright (C) 2014 Lilaea Media
 */
 
     defined('LF') or define('LF', "\n");
