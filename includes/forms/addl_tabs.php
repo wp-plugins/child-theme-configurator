7dc295bb7759628ef7fbd625b3262a1796950e94
lilaeamedia <lilaeamedia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-21 02:01:06 +0000

    update tabs
    git-svn-id: https://plugins.svn.wordpress.org/child-theme-configurator/tags/1.7.0@1095647 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/forms/addl_tabs.php
+++ includes/forms/addl_tabs.php
@@ -5,7 +5,7 @@ if ( !defined( 'CHLD_THM_CFG_PLUGINS_VERSION' ) ):
 ?>
 <a id="get_pro" href="?page=<?php echo CHLD_THM_CFG_MENU; ?>&amp;tab=get_pro" 
                     class="nav-tab<?php echo 'get_pro' == $active_tab ? ' nav-tab-active' : ''; ?>" <?php echo $hidechild; ?>>
-<?php _e( 'Get CTC Pro', 'chld_thm_cfg_plugins' ); ?>
+<?php _e( 'Upgrade Now', 'chld_thm_cfg_plugins' ); ?>
 </a>
 <?php
 endif;
