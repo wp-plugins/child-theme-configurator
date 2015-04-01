4642506a6e095da4c727692db355f6f0a89d5884
lilaeamedia <lilaeamedia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-01 12:57:23 +0000

    update rev num
    git-svn-id: https://plugins.svn.wordpress.org/child-theme-configurator/trunk@1125345 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/chld-thm-cfg.js
+++ js/chld-thm-cfg.js
@@ -2,7 +2,7 @@
  *  Script: chld-thm-cfg.js
  *  Plugin URI: http://www.childthemeconfigurator.com/
  *  Description: Handles jQuery, AJAX and other UI
- *  Version: 1.7.3.1
+ *  Version: 1.7.3.2
  *  Author: Lilaea Media
  *  Author URI: http://www.lilaeamedia.com/
  *  License: GPLv2
